0e3fbc4a78046db00888d924b854f0a5e8e81c61
于洪洋 <97099566+yhy0816@users.noreply.github.com>
2023-05-22 00:29:32 +0800

    Add files via upload

---
+++ a.js
@@ -0,0 +1,38 @@
+/*页面一（主页）js代码：*/function myFunction1(){
+    var x="欢迎来到我的页面！"
+document.getElementById("h1").innerHTML=x;
+var y="welcome to my personal webpage"
+document.getElementById("p1").innerHTML=y;
+}
+
+function myFunction2()
+{
+   /*' const x=<a href="https://www.imooc.com/">n</a>
+   */ const element = document.getElementById("aintroduce").href;
+var x="点击左侧“我的简介”即可进入我的介绍"
+document.getElementById("h1").innerHTML=x;
+var y="My introduce"
+document.getElementById("p1").innerHTML=y;
+}
+
+function myFunction3()
+{
+   /*' const x=<a href="https://www.imooc.com/">n</a>
+   */ const element = document.getElementById("aintroduce").href;
+var x="点击左侧“我的相册”查看我的相册"
+document.getElementById("h1").innerHTML=x;
+var y="My photo"
+document.getElementById("p1").innerHTML=y;
+}
+
+function myFunction4()
+{
+   /*' const x=<a href="https://www.imooc.com/">n</a>
+   */ const element = document.getElementById("aintroduce").href;
+var x="点击左侧“心情随笔”即可进入"
+document.getElementById("h1").innerHTML=x;
+var y="My mood"
+document.getElementById("p1").innerHTML=y;
+}
+
+
